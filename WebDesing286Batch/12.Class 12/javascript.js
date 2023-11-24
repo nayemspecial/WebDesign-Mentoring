08831960ddffd3b9adc23304e717e4410338e3b2
Md. Nayemur Rahman <nayemspecial@gmail.com>
2023-11-24 22:47:43 +0600

    Add files via upload

---
+++ WebDesing286Batch/12.Class 12/javascript.js
@@ -0,0 +1,27 @@
+// $(document).ready(function(){
+            
+//     $(".hide").click(function(){
+//         $("h1").hide();
+//     });
+
+//     $(".show").click(function(){
+//         $("h1").show();
+//     });
+
+//     $('.owl-carousel').owlCarousel(function(){
+//         items:1,
+//         loop:true,
+//         nav:true
+//     });
+
+// });
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        nav:true,
+        items:1,
+        autoplay:true
+    });
+});
+
